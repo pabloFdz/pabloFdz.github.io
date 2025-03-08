39974b46c1593eb32e0855fb08615ba0f3525696
pabloFdz <15377889+pabloFdz@users.noreply.github.com>
2025-03-08 01:41:20 +0100

    Remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://pablofdz.github.io',
-  base: 'pablofdz.github.io'
+  site: 'https://pablofdz.github.io'
 });
