d31f4b5559cfc25cff10ecc0dc76df99533382a5
pabloFdz <15377889+pabloFdz@users.noreply.github.com>
2025-03-08 15:43:26 +0100

    Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+      tailwind(),
+      mdx(),
+      sitemap(),
+      partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      })
+  ],
   site: 'https://pablofdz.github.io'
 });
